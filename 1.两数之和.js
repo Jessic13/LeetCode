654838c755b8e91e136c440d6180f305ba569b84
jessic13 <963820036@qq.com>
2023-02-19 21:01:32 +0800

    code language:js

---
+++ 1.两数之和.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var a=1;
+    var b=2;
+    console.log(a);
+    console.log(b);
+};
+
+twoSum();
+// @lc code=end
