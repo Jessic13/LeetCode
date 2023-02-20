4443c72d48e52822abcb3f23df5871b7032c641f
jessic13 <963820036@qq.com>
2023-02-20 12:49:48 +0800

    第一题：map最优解

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,12 +10,20 @@
  * @param {number} target
  * @return {number[]}
  */
+// 动态规划
 var twoSum = function(nums, target) {
-    var a=1;
-    var b=2;
-    console.log(a);
-    console.log(b);
+    var map=new Map();
+    for(i=0;i<nums.length;i++){
+        x=target-nums[i];
+        if(map.has(x)){
+            return [map.get(x),i];
+        }
+        map.set(nums[i],i);
+    }
+    
 };
 
-twoSum();
 // @lc code=end
+// @after-stub-for-debug-begin
+module.exports = twoSum;
+// @after-stub-for-debug-end
