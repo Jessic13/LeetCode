c97f174345b7ec99a3cb60514ae59a6d38c1316f
jessic13 <963820036@qq.com>
2023-03-29 22:38:36 +0800

    第七周

---
+++ 6.n-字形变换.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] N 字形变换
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+/**
+ * 利用二维矩阵模拟
+ * 初始化并获取如题所示数组，遍历即可
+ */
+var convert = function(s, numRows) {
+  
+};
+// @lc code=end
+
